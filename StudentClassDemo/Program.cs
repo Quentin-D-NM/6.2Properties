23111df39bd3307d14e81398d6ae461072391bf3
Quentin-D-NM <qdye2001@gmail.com>
2021-06-30 16:15:58 -0600

    Initial Commit

---
+++ StudentClassDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StudentClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student molly = new Student("Molly Cat", 1234);
+
+            Console.WriteLine("molly's Name: {0}", molly.Name);
+            Console.WriteLine("molly's Id Number: {0}", molly.IdNumber);
+            molly.Score1 = 100;
+            molly.Score2 = 80;
+            Console.WriteLine("molly's Average Score: {0}", molly.AverageScore);
+            Console.WriteLine("molly's High Score: {0}", molly.HighScore);
+
+            Student bella = new Student("Bella");
+
+            Console.WriteLine("bella's Id Number: {0}", bella.IdNumber);
+            Console.WriteLine("bella's Name: {0}", bella.Name);
+
+            Student kitkat = new Student("Kitkat");
+
+            Console.WriteLine("kitkat's Id Number: {0}", kitkat.IdNumber);
+            Console.WriteLine("kitkat's Name: {0}", kitkat.Name);
+
+
+        }
+    }
+}
---
+++ StudentClassDemo/Student.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentClassDemo
+{
+    class Student
+    {
+        private int _score1;
+        private int _score2;
+       
+        public string Name { get; set; }
+        public string IdNumber { get; set; }
+        public double AverageScore { get; set; }
+        public int Score1 { get { return _score1; } set { _score1 = value;  UpdateAverageScore(); } }
+        public int Score2 { get { return _score2; } set { _score2 = value; UpdateAverageScore(); } }
+        public double HighScore { 
+            get
+            {
+                if (Score1 > Score2)
+                {
+                    return Score1;
+                }
+                else
+                {
+                    return Score2;
+                }
+            } 
+        }
+
+
+        public Student() : this("Unknown", -1)
+        {
+
+        }
+
+        public Student(int newIdNumber) : this("Unknown", newIdNumber)
+        {
+        }
+
+        public Student(string newName) : this(newName, -1)
+        {
+        }
+
+        public Student(string newName, int newIdNumber) 
+        {
+            this.Name = newName;
+            this.IdNumber = newIdNumber.ToString(); 
+        }
+
+        public static void DoStuff()
+        {
+            Student molly = new Student();
+            molly.Name = "Molly";
+            molly.IdNumber = 42.ToString();
+            Console.WriteLine(molly.Name);
+            Console.WriteLine(molly.IdNumber);
+
+            Student someStudent = new Student();
+            Console.WriteLine("someStudent: {0}", someStudent.Name);
+            Console.WriteLine("someStudent: {0}", someStudent.IdNumber);
+
+
+
+        }
+
+        private void UpdateAverageScore()
+        {
+            AverageScore = (Score1 + Score2) / 2; 
+        }
+
+
+    }
+}
